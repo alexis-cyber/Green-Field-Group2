e8b5c82c94a87970460ec5744c7adeebd1e9b9cc
ChrysanthiNouli <chrysanthin@hotmail.com>
2023-10-20 16:46:00 +0300

    stucture and form correction

--- Pantry/myapp/src/App.js
+++ Pantry/myapp/src/App.js
@@ -1,8 +1,11 @@
+import Form from "../src/components/Form";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
   return (
     <div className="App">
-  
+      
     </div>
   );
 }
--- Pantry/myapp/src/index.js
+++ Pantry/myapp/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import App from './App';
+import ReactDOM from 'react-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -8,4 +9,3 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
